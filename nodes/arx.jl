e65d87f8f4bf4fcaec54af8600b71ade94a7733d
wmkouw <wmkouw@gmail.com>
2024-03-18 21:48:28 +0100

    Worked on ARX node messages.

--- nodes/arx.jl
+++ nodes/arx.jl
@@ -5,9 +5,10 @@ struct ARX end
 
 @rule ARX(:out, Marginalisation) (q_x::PointMass, q_ζ::MvNormalGamma) = begin
 
-
+    mx = mean(q_x)
+    μ,Λ,α,β = params(q_ζ)
     
-    return NormalMeanPrecision(out_mean, out_prec)
+    return NormalMeanPrecision(μ'*mx, mx'*inv(α/β*Λ)*mx + β/α)
 end
 
 @rule ARX(:x, Marginalisation) (q_out::PointMass, q_ζ::MvNormalGamma) = begin
@@ -16,6 +17,14 @@ end
 end
 
 @rule ARX(:ζ, Marginalisation) (q_out::PointMass, q_x::PointMass) = begin
+
+    mx = mean(q_x)
+    my = mean(q_out)
+
+    μ = inv(mx*mx')*(mx*my)
+    Λ = mx*mx'
+    α = 1/2
+    β = 1/2*(my^2 - (mx*my)'*inv(mx*mx')*(mx*my))
     
-    return MvNormalGamma()
+    return MvNormalGamma(μ,Λ,α,β)
 end
